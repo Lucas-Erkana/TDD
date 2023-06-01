a43f5f6f5c3a7734ff67eff70839bb23324fee24
Lucas David Erkana <lderkana21x@gmail.com>
2023-06-01 08:42:45 +0200

    Update solver_spec.rb

--- solver_spec.rb
+++ solver_spec.rb
@@ -5,18 +5,18 @@ RSpec.describe Solver do
     @solver = Solver.new
   end
 
-  describe "#factorial" do
-    it "returns 1 when given 0" do
+  describe '#factorial' do
+    it 'returns 1 when given 0' do
       expect(@solver.factorial(0)).to eq(1)
     end
 
-    it "returns the correct factorial for positive integers" do
+    it 'returns the correct factorial for positive integers' do
       expect(@solver.factorial(5)).to eq(120)
       expect(@solver.factorial(6)).to eq(720)
-      expect(@solver.factorial(10)).to eq(3628800)
+      expect(@solver.factorial(10)).to eq(3_628_800)
     end
 
-    it "raises an ArgumentError when given a negative integer" do
+    it 'raises an ArgumentError when given a negative integer' do
       expect { @solver.factorial(-5) }.to raise_error(ArgumentError)
     end
   end
